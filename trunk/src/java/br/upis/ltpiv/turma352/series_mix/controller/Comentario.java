75fee032431d0cbb93c0df79b2ae7aa8f258db44
fco.ernesto <fco.ernesto@b2a46a6f-a57f-00d5-e7d9-0ed22957c034>
2012-04-27 09:12:46 +0000

    Envio do estilo.

---
+++ trunk/src/java/br/upis/ltpiv/turma352/series_mix/controller/Comentario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.upis.ltpiv.turma352.series_mix.controller;
+
+import br.upis.ltpiv.turma352.series_mix.model.DefaultModelOperation;
+
+/**
+ *
+ * @author francisco.teixeira
+ */
+public class Comentario extends DefaultModelOperation {
+    
+    public Comentario() {
+        this.defaultTable = "Comentario";
+    }
+        
+}
